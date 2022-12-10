ee81517db6345c70889f7ce38a0a4c58552b07ef
lisiza <79217466052@yandex.ru>
2022-12-10 23:02:22 +0300

    fixed second  review comments

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const buttonsClosePopup = document.querySelectorAll('.popup__close-button');
 const buttonOpenProfilePopup = document.querySelector('.profile__edit-button');
 const popupEditProfile = document.querySelector('.popup_edit-profile');
-const forms = document.querySelectorAll('.form')
 const formEditProfile = document.getElementById('profile-edit-form');
 const inputName = formEditProfile.querySelector('.form__input_el_name');
 const inputJob = formEditProfile.querySelector('.form__input_el_caption');
@@ -18,47 +17,48 @@ const elementForm = document.getElementById('image-add-form');
 const inputPlaceName = elementForm.querySelector('.form__input_el_name');
 const inputPlaceImage = elementForm.querySelector('.form__input_el_caption');
 
-
-//универсальный (ну, почти) открыватель попапов
 function openPopup (element) {
   element.classList.add('popup_opened');
-  if (element == popupEditProfile) {
-    inputName.placeholder = currentName.textContent; //вписываем в плейсхолдер текущее имя
-    inputJob.placeholder = currentJob.textContent; //вписываем в плейсхолдер текущую профессию
-  }
-  if (element == popupAddImage || element == popupEditProfile) {
-    const currentForm = element.querySelector('form');
-    currentForm.reset();
-  }
 }
 
-//универсальный закрыватель попапов
-function closePopup (element) {
-  const targetPopup = element.target.parentNode.parentNode;
-  targetPopup.classList.remove('popup_opened');
+function closePopup () {
+  const popupOpened = document.querySelector('.popup_opened');
+  popupOpened.classList.remove('popup_opened');
 }
 
-//универсальный обработчик форм
-function submitForm(evt) {
+function resetForm (element) {
+  const currentForm = element.querySelector('form');
+  currentForm.reset();
+}
+
+function submitformEditProfile (evt) {
   evt.preventDefault();
-  if (evt.target == formEditProfile) {
-    currentName.textContent = inputName.value; //записываем в профиль введенное имя
-    currentJob.textContent = inputJob.value; //записываем в профиль введенную профессию
-  } else if (evt.target == elementForm) {
-    elementsList.prepend(createElement (inputPlaceImage.value, inputPlaceName.value))
-  }
-  evt.target.parentNode.parentNode.classList.remove('popup_opened');
+  currentName.textContent = inputName.value;
+  currentJob.textContent = inputJob.value;
+  closePopup();
+}
+
+function submitElementForm (evt) {
+  evt.preventDefault();
+  elementsList.prepend(createElement (inputPlaceImage.value, inputPlaceName.value));
+  closePopup();
 }
 
 
-//генератор фотографий на странице при открытии
 function addElements (arr) {
   arr.forEach( function (element) {
     elementsList.append(createElement (element.link, element.name))
   })
 }
 
-//функция, создающая одну новую фоточку
+function moveToTrash (element) {
+  element.remove();
+}
+
+function toggleLike (element) {
+  element.classList.toggle('element__like-button_active');
+}
+
 function createElement (image, title) {
   const newElement = elementTemplate.content.cloneNode(true);
   const elementImage = newElement.querySelector('.element__image');
@@ -68,33 +68,47 @@ function createElement (image, title) {
   elementTitle.textContent = title;
   elementImage.setAttribute('alt', "Фото, добавленное пользователем: " + title);
 
-  const like = newElement.querySelector('.element__like-button'); //добавляем возможность лайкать эту фоточку
-  like.addEventListener('click', () => like.classList.toggle('element__like-button_active'));
+  const like = newElement.querySelector('.element__like-button');
+  like.addEventListener('click', () => toggleLike(like));
 
-  const moveToTrash = newElement.querySelector('.element__trash-button'); //добавляем возможность удалить эту фоточку
-  moveToTrash.addEventListener('click', () => moveToTrash.parentNode.parentNode.remove()) //удаляем прародителя, иначе в гриде будут дырки
+  const iconToTrash = newElement.querySelector('.element__trash-button');
+  iconToTrash.addEventListener('click', () => {
+    elementToTrash = iconToTrash.closest('li');
+    moveToTrash(elementToTrash);
+  })
 
   elementImage.addEventListener('click', () => {
-    openPopup(popupViewImage); //открываем картинку на весь экран по клику
-    popupViewImageImage.src = elementImage.src; //и передаем в попап изображение, по которому кликнули
-    popupViewImageTitle.textContent = elementTitle.textContent; //и его название
+    popupViewImageImage.src = elementImage.src;
+    popupViewImageTitle.textContent = elementTitle.textContent;
+    openPopup(popupViewImage);
   });
 
   return newElement
 };
 
 
-buttonOpenProfilePopup.addEventListener('click', () => openPopup(popupEditProfile));
-//мне тоже не нравится, что две очень похожие функции, но как их объединить не придумала
-buttonAddImage.addEventListener('click', () => openPopup(popupAddImage));
-
-buttonsClosePopup.forEach(buttonClosePopup => buttonClosePopup.addEventListener('click', (evt) => closePopup(evt)));
-
-
-forms.forEach (function (form) {
-  form.addEventListener('submit', submitForm)
+buttonOpenProfilePopup.addEventListener('click', () => {
+  inputName.value = currentName.textContent;
+  inputJob.value = currentJob.textContent;
+  resetForm(popupEditProfile);
+  openPopup(popupEditProfile);
 })
 
+buttonAddImage.addEventListener('click', () => {
+  resetForm(popupAddImage);
+  openPopup(popupAddImage);
+});
 
-//заполняем страницу фотографиями при открытии
+
+buttonsClosePopup.forEach(buttonClosePopup =>
+  buttonClosePopup.addEventListener('click', (evt) => {
+    const popupOpened = document.querySelector('.popup_opened');
+    closePopup(popupOpened);
+  }));
+
+
+formEditProfile.addEventListener('submit', (evt) => submitformEditProfile(evt));
+elementForm.addEventListener('submit', (evt) => submitElementForm(evt));
+
+
 addElements(initialCards);
