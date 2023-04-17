991471a3c4ed90f1c52344d062b5de3bbd4aa538
lisiza <79217466052@yandex.ru>
2023-04-17 20:42:56 +0300

    remoced closing popup by esc error

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const buttonsClosePopup = document.querySelectorAll('.popup__close-button');
-const buttonOpenProfilePopup = document.querySelector('.profile__edit-button');
-const popupEditProfile = document.querySelector('.popup_edit-profile');
-const formEditProfile = document.getElementById('profile-edit-form');
-const inputName = formEditProfile.querySelector('.form__input_el_name');
-const inputJob = formEditProfile.querySelector('.form__input_el_caption');
-const currentName = document.querySelector('.profile__name');
-const currentJob = document.querySelector('.profile__caption');
 const buttonAddImage = document.querySelector('.profile__add-button');
+const buttonOpenProfilePopup = document.querySelector('.profile__edit-button');
+const buttonsClosePopup = document.querySelectorAll('.popup__close-button');
+const currentJob = document.querySelector('.profile__caption');
+const currentName = document.querySelector('.profile__name');
+const elementForm = document.getElementById('image-add-form');
+const elementsList = document.querySelector('.elements__list');
+const elementTemplate = document.getElementById('element-template');
+const formEditProfile = document.getElementById('profile-edit-form');
+const iconToTrash = document.querySelector('.element__trash-button');
+const inputJob = formEditProfile.querySelector('.form__input_el_caption');
+const inputName = formEditProfile.querySelector('.form__input_el_name');
+const inputPlaceImage = elementForm.querySelector('.form__input_el_caption');
+const inputPlaceName = elementForm.querySelector('.form__input_el_name');
+const like = document.querySelector('.element__like-button');
 const popupAddImage = document.querySelector('.popup_add-image');
+const popupEditProfile = document.querySelector('.popup_edit-profile');
 const popupViewImage = document.querySelector('.popup_view-image');
 const popupViewImageImage = document.querySelector('.popup__image');
 const popupViewImageTitle = document.querySelector('.popup__title');
-const elementTemplate = document.getElementById('element-template');
-const elementsList = document.querySelector('.elements__list');
-const elementForm = document.getElementById('image-add-form');
-const inputPlaceName = elementForm.querySelector('.form__input_el_name');
-const inputPlaceImage = elementForm.querySelector('.form__input_el_caption');
-const like = document.querySelector('.element__like-button');
-const iconToTrash = document.querySelector('.element__trash-button');
 
 
 function openPopup (element) {
@@ -39,7 +39,9 @@ function closePopupByEsc(evt) {
 
 function closePopup () {
   popup = document.querySelector('.popup_opened')
-  popup.classList.remove('popup_opened');
+  if (popup) {
+    popup.classList.remove('popup_opened');
+  }
 }
 
 function resetForm (element) {
